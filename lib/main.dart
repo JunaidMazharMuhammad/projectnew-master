1192376ec356e24aafb45d50494b7fc2acd97e0e
nosherwan.ijaz <nosherwan.ijaz@careem.com>
2019-04-20 13:55:45 +0500

    _center updated at start of application

--- lib/main.dart
+++ lib/main.dart
@@ -34,13 +34,13 @@ class _MyAppState extends State<MyApp> {
 
   GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
 
-  static  LatLng _center =  LatLng(24.7732, 67.0762);
+  static  LatLng _center =  LatLng(24.773, 67.0762);
 
 
   final Set<Marker> _markers = {};
 
 
-   static  LatLng _lastMapPosition = _center;
+  static  LatLng _lastMapPosition = _center;
 
 
   MapType _currentMapType = MapType.normal;
@@ -62,7 +62,94 @@ class _MyAppState extends State<MyApp> {
 
   }
 
-  Future<void> _getLocation() async {
+  Future<void> _setLocation() async {
+    Map<dynamic, dynamic> locationMap;
+    String result;
+    try {
+      locationMap = await GeoLocation.getLocation;
+
+      var status = locationMap["status"];
+      if ((status is String && status == "true") ||
+          (status is bool) && status) {
+        var lat = locationMap["latitude"];
+        var lng = locationMap["longitude"];
+        if (lat is String) {
+          result = "Location: ($lat, $lng)";
+
+          _center=LatLng(double.parse(lat),double.parse(lng));
+
+//          print(double.parse(lat));
+        } else {
+          // lat and lng are not string, you need to check the data type and use accordingly.
+          // it might possible that else will be called in Android as we are getting double from it.
+          _center=LatLng(lat, lng);
+          result = "Location: ($lat, $lng)";
+        }
+      } else {
+        result = locationMap["message"];
+      }
+
+    }
+    catch(e){
+
+    }
+
+  }
+
+
+
+
+  void _onAddMarkerButtonPressed() async {
+
+
+    var location = Location(_center.latitude,_center.longitude);
+
+    final result = await _places.searchByText("Masjid",location: location, radius: 1000,type: "Masjid");
+    setState(() {
+      result.results.forEach((f) {
+
+        this.places = result.results;
+
+        LatLng newloc= LatLng(f.geometry.location.lat, f.geometry.location.lng);
+        print(f.name);
+
+
+        _lastMapPosition=newloc;
+        print(f.geometry.location.lat.toString() + f.geometry.location.lng.toString());
+        _markers.add(Marker(
+
+
+          // This marker id can be anything that uniquely identifies each marker.
+
+          markerId: MarkerId(_lastMapPosition.toString()),
+
+          position:  _lastMapPosition,
+
+          infoWindow: InfoWindow(
+
+            title: f.name,
+
+            snippet:f.formattedAddress,
+
+
+
+          ),
+
+          icon: BitmapDescriptor.defaultMarker,
+        ));
+      });
+
+    });
+  }
+
+
+
+  void _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+
+  Future _onMapCreated(GoogleMapController controller) async {
     Map<dynamic, dynamic> locationMap;
     String result;
     try {
@@ -93,181 +180,128 @@ class _MyAppState extends State<MyApp> {
 
     }
 
+
+
+
+     _controller.complete(controller);
+
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: Scaffold(
+
+        appBar: AppBar(
+
+          title: Text('Maps Sample App'),
+
+          backgroundColor: Colors.green[700],
+
+        ),
+
+        body: Stack(
+
+          children: <Widget>[
+
+            GoogleMap(
+
+              onMapCreated: _onMapCreated,
 
 
+              initialCameraPosition: CameraPosition(
 
-  void _onAddMarkerButtonPressed() async {
-    
+                target: _center,
 
-    var location = Location(_center.latitude,_center.longitude);
+                zoom: 10.0,
 
-    final result = await _places.searchByText("Masjid",location: location, radius: 1000,type: "Masjid");
-    setState(() {
-      result.results.forEach((f) {
+              ),
 
-            this.places = result.results;
+              mapType: _currentMapType,
 
-            LatLng newloc= LatLng(f.geometry.location.lat, f.geometry.location.lng);
-            print(f.name);
+              markers: _markers,
 
+              onCameraMove: _onCameraMove,
 
-          _lastMapPosition=newloc;
-            print(f.geometry.location.lat.toString() + f.geometry.location.lng.toString());
-          _markers.add(Marker(
-
-
-            // This marker id can be anything that uniquely identifies each marker.
-
-            markerId: MarkerId(_lastMapPosition.toString()),
-
-            position:  _lastMapPosition,
-
-            infoWindow: InfoWindow(
-
-              title: f.name,
-
-              snippet:f.formattedAddress,
-
-
+              myLocationEnabled: true,
 
             ),
 
-            icon: BitmapDescriptor.defaultMarker,
-          ));
-        });
+            Padding(
 
-    });
-  }
+              padding: const EdgeInsets.all(16.0),
 
+              child: Align(
 
+                alignment: Alignment.topRight,
 
-      void _onCameraMove(CameraPosition position) {
-        _lastMapPosition = position.target;
-      }
-
-
-      void _onMapCreated(GoogleMapController controller) {
-        _controller.complete(controller);
-        
-      }
-
-      @override
-      Widget build(BuildContext context) {
-        return MaterialApp(
-
-          home: Scaffold(
-
-            appBar: AppBar(
-
-              title: Text('Maps Sample App'),
-
-              backgroundColor: Colors.green[700],
-
-            ),
-
-            body: Stack(
-
-              children: <Widget>[
-
-                GoogleMap(
-
-                  onMapCreated: _onMapCreated,
-
-
-                  initialCameraPosition: CameraPosition(
-
-                    target: _center,
-
-                    zoom: 17.0,
-
-                  ),
-
-                  mapType: _currentMapType,
-
-                  markers: _markers,
-
-                  onCameraMove: _onCameraMove,
-
-                  myLocationEnabled: true,
-
-                ),
-
-                Padding(
-
-                  padding: const EdgeInsets.all(16.0),
-
-                  child: Align(
-
-                    alignment: Alignment.topRight,
-
-                    child: Column(
+                child: Column(
 
 
 
-                      children: <Widget>[
+                  children: <Widget>[
 
 
-                         FloatingActionButton(
+                    FloatingActionButton(
 
-                          onPressed: _onMapTypeButtonPressed,
+                      onPressed: _onMapTypeButtonPressed,
 
-                          materialTapTargetSize: MaterialTapTargetSize.padded,
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
 
-                          backgroundColor: Colors.green,
+                      backgroundColor: Colors.green,
 
-                          child: const Icon(Icons.map, size: 36.0),
+                      child: const Icon(Icons.map, size: 36.0),
 
-
-                        ),
-
-                        SizedBox(height: 16.0),
-
-                        FloatingActionButton(
-
-                          onPressed: _onAddMarkerButtonPressed,
-
-                          materialTapTargetSize: MaterialTapTargetSize.padded,
-
-                          backgroundColor: Colors.green,
-
-                          child: const Icon(Icons.add_location, size: 36.0),
-
-                        ),
-
-                         SizedBox(height: 16.0),
-
-                         FloatingActionButton(
-                          onPressed: _getLocation,
-
-
-                           materialTapTargetSize: MaterialTapTargetSize.padded,
-
-                           backgroundColor: Colors.green,
-
-                           child: const Icon(Icons.near_me, size: 36.0),
-
-
-                         ),
-
-
-                      ],
 
                     ),
 
-                  ),
+                    SizedBox(height: 16.0),
+
+                    FloatingActionButton(
+
+                      onPressed: _onAddMarkerButtonPressed,
+
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+
+                      backgroundColor: Colors.green,
+
+                      child: const Icon(Icons.add_location, size: 36.0),
+
+                    ),
+
+                    SizedBox(height: 16.0),
+
+                    FloatingActionButton(
+                      onPressed: _setLocation,
+
+
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+
+                      backgroundColor: Colors.green,
+
+                      child: const Icon(Icons.near_me, size: 36.0),
+
+
+                    ),
+
+
+                  ],
 
                 ),
 
-              ],
+              ),
 
             ),
 
-          ),
+          ],
 
-        );
+        ),
 
-      }
+      ),
 
-    }
+    );
+
+  }
+
+}
